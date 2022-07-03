3004e38da000f740c4754cc771cb319c74529f6f
Sunwoo Kim <51080767+sunwoodk@users.noreply.github.com>
2022-07-03 23:35:35 +0900

    Add files via upload

---
+++ FlyApp/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ModelCycleDesignForm modelCycleDesignForm = new ModelCycleDesignForm();
+            modelCycleDesignForm.Show();
+        }
+
+       
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PlannerCamDesignForm plannerCamDesignForm = new PlannerCamDesignForm();
+            plannerCamDesignForm.Show();
+        }
+
+       
+    }
+}
